64bc563d815d8f367c1bf121a030f8ac1a5b6f0c
Yasiralam <yasiralam981@gmail.com>
2025-01-27 22:33:17 +0530

    added TaskListRepository, TaskRepository

---
+++ tasks/src/main/java/com/yasir/tasks/repositories/TaskListRepository.java
@@ -0,0 +1,13 @@
+package com.yasir.tasks.repositories;
+
+import com.yasir.tasks.domain.entities.Task;
+import com.yasir.tasks.domain.entities.TaskList;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TaskListRepository  extends JpaRepository<TaskList, UUID> {
+
+}
